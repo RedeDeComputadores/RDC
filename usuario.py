a976d4429cf4ef696ce57c0982aaf725bf9ae118
Edson <edson@edson-OEM.(none)>
2014-03-09 16:00:37 -0300

    Commit alteraçao 1

--- usuario.py
+++ usuario.py
@@ -1,10 +1,27 @@
-import  unittest as u
-from should_dsl import should, should_not
+#encoding: utf-8
 
-class Usuario (object):
-	def __init__(self, nome_usuario, senha):
-		self.nome_usuario = nome_usuario
+from datetime import datetime
+from conexao import Conexao
+from impressora import ErroConexao
+
+class Usuario(object):
+	def __init__(self, nome_de_guerra, senha):
+		self.nome_de_guerra = nome_de_guerra
 		self.senha = senha
-		
-if(__name__=='__main__'):
-	u.main()
+		self.esta_conectado = False
+
+	def conectar(self):
+		data = "%d/%d/%d" %(datetime.today().day, datetime.today().month, datetime.today().year)
+		hora = "%d:%d" %(datetime.today().hour, datetime.today().minute)
+		self.conexao = Conexao(self.nome_de_guerra, data=data, hora_inicio=hora)
+		self.esta_conectado = True
+
+	def enviar_arquivo(self, arquivo_enviado, copias):
+		if self.esta_conectado == False:
+			raise ErroConexao('Usuário não conectado!')
+		else:
+			if arquivo_enviado.nome in self.conexao.arquivos_enviados.keys():
+				self.conexao.arquivos_enviados[arquivo_enviado.nome] += copias
+			else:
+				self.conexao.arquivos_enviados.update({arquivo_enviado.nome: copias})
+
